20ed739f2595663af2c26b5b2849638e3ed1e20e
bahmi <bahadurmisho@gmail.com>
2019-09-20 21:32:25 +0600

    Add top headlines for technology news sources

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sources/ars-technica', views.ars_technica, name='ars_technica'),
+    path('sources/crypto-coins-news', views.crypto_coins_news, name='crypto_coins_news'),
+    path('sources/engadget', views.engadget, name='engadget'),
+    path('sources/hacker-news', views.hacker_news, name='hacker_news'),
+    path('sources/recode', views.recode, name='recode'),
+    path('sources/techcrunch', views.techcrunch, name='techcrunch'),
+    path('sources/techradar', views.techradar, name='techradar'),
+    path('sources/the-next-web', views.the_next_web, name='techradar'),
+    path('sources/the-verge', views.the_verge, name='the_verge'),
+    path('sources/wired', views.wired, name='wired'),
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from decouple import config
 from newsapi import NewsApiClient
 
-# Create your views here.
+api_request = NewsApiClient(api_key=config('api_key'))
+
 def home(request):
-    api_request = NewsApiClient(api_key=config('api_key'))
     technology_src = api_request.get_sources(category='technology', language='en')
     sports_src = api_request.get_sources(category='sports', language='en')
     business_src = api_request.get_sources(category='business', language='en')
@@ -15,3 +15,73 @@ def home(request):
                                          'business_src': business_src,
                                          'entertainment_src': entertainment_src,
                                          })
+
+def ars_technica(request):
+    page_title = 'Ars Technica'
+    ars_technica = api_request.get_top_headlines(sources='ars-technica', language='en')
+    return render(request, 'ars_technica.html', {'ars_technica': ars_technica, 'page_title': page_title})
+
+def crypto_coins_news(request):
+    page_title = 'Crypto Coins News'
+    crypto_coins_news = api_request.get_top_headlines(sources='crypto-coins-news', language='en')
+    return render(request, 'crypto_coins_news.html', {'crypto_coins_news': crypto_coins_news,
+                                                      'page_title': page_title})
+
+def engadget(request):
+    page_title = 'Engadget'
+    engadget = api_request.get_top_headlines(sources='engadget', language='en')
+    return render(request, 'engadget.html', {'engadget': engadget,
+                                             'page_title': page_title})
+
+def hacker_news(request):
+    page_title = 'Hacker News'
+    hacker_news = api_request.get_top_headlines(sources='hacker-news', language='en')
+    return render(request, 'hacker_news.html', {'hacker_news': hacker_news,
+                                                'page_title': page_title})
+
+def recode(request):
+    page_title = 'Recode'
+    recode = api_request.get_top_headlines(sources='recode', language='en')
+    return render(request, 'recode.html', {'recode': recode,
+                                           'page_title': page_title})
+
+def techcrunch(request):
+    page_title = 'TechCrunch'
+    techcrunch = api_request.get_top_headlines(sources='techcrunch', language='en')
+    return render(request, 'techcrunch.html', {'techcrunch': techcrunch,
+                                               'page_title': page_title})
+
+def techradar(request):
+    page_title = 'TechRadar'
+    techradar = api_request.get_top_headlines(sources='techradar', language='en')
+    return render(request, 'techradar.html', {'techradar': techradar,
+                                              'page_title': page_title})
+
+def the_next_web(request):
+    page_title = 'The Next Web'
+    the_next_web = api_request.get_top_headlines(sources='the-next-web', language='en')
+    return render(request, 'the_next_web.html', {'the_next_web': the_next_web,
+                                                 'page_title': page_title})
+
+def the_verge(request):
+    page_title = 'The Verge'
+    the_verge = api_request.get_top_headlines(sources='the-verge', language='en')
+    return render(request, 'the_verge.html', {'the_verge': the_verge,
+                                               'page_title': page_title})
+
+def wired(request):
+    page_title = 'Wired'
+    wired = api_request.get_top_headlines(sources='wired', language='en')
+    return render(request, 'wired.html', {'wired': wired,
+                                               'page_title': page_title})
+
+
+
+
+
+
+
+
+
+
+
