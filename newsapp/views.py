e8cc5da74033d1cb5de4e801c215a04796d95863
bahmi <bahadurmisho@gmail.com>
2019-09-20 18:03:20 +0600

    Add few other news sources

--- newsapp/views.py
+++ newsapp/views.py
@@ -5,6 +5,13 @@ from newsapi import NewsApiClient
 # Create your views here.
 def home(request):
     api_request = NewsApiClient(api_key=config('api_key'))
-    business_sources = api_request.get_sources(category='business', language='en')
-    return render(request, 'home.html', {'api_request':api_request,
-                                         'business_sources': business_sources})
+    technology_src = api_request.get_sources(category='technology', language='en')
+    sports_src = api_request.get_sources(category='sports', language='en')
+    business_src = api_request.get_sources(category='business', language='en')
+    entertainment_src = api_request.get_sources(category='entertainment', language='en')
+
+    return render(request, 'home.html', {'technology_src': technology_src,
+                                         'sports_src': sports_src,
+                                         'business_src': business_src,
+                                         'entertainment_src': entertainment_src,
+                                         })
